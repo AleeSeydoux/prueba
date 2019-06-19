04c0df33997a613e4397645820435a582e55c4f4
Ale Seydoux <aleevelarde9@gmail.com>
2019-06-19 11:55:47 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mayra Osorio
+        Intento 2 de remoto
         </p>
         <a
           className="App-link"
